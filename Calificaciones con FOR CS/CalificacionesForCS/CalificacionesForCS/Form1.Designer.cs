7dc962e4730618a782afcb71f776f5a607624489
data006 <109679683+data006@users.noreply.github.com>
2022-07-20 12:02:28 -0600

    Add files via upload

---
+++ Calificaciones con FOR CS/CalificacionesForCS/CalificacionesForCS/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CalificacionesForCS
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnOK = new System.Windows.Forms.Button();
+			this.txtCantidad = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnOK
+			// 
+			this.btnOK.Location = new System.Drawing.Point(152, 74);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(75, 23);
+			this.btnOK.TabIndex = 3;
+			this.btnOK.Text = "OK";
+			this.btnOK.UseVisualStyleBackColor = true;
+			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+			// 
+			// txtCantidad
+			// 
+			this.txtCantidad.Location = new System.Drawing.Point(142, 48);
+			this.txtCantidad.Name = "txtCantidad";
+			this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+			this.txtCantidad.TabIndex = 2;
+			this.txtCantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCantidad_KeyPress);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(39, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(324, 20);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Cantidad de calificaciones a promediar:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Yellow;
+			this.ClientSize = new System.Drawing.Size(392, 155);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnOK);
+			this.Controls.Add(this.txtCantidad);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		internal System.Windows.Forms.Button btnOK;
+		internal System.Windows.Forms.TextBox txtCantidad;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ Calificaciones con FOR CS/CalificacionesForCS/CalificacionesForCS/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace CalificacionesForCS
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+
+		private int numCantidad;
+		private double numInput;
+		private double numProm;
+		private string numEntrada;
+
+
+		private void txtCantidad_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if ((txtCantidad.Text ?? "") == (string.Empty ?? ""))
+			{
+				e.Handled = !char.IsDigit(e.KeyChar) & !char.IsControl(e.KeyChar) & !(Conversions.ToString(e.KeyChar) == ".") & !(Conversions.ToString(e.KeyChar) == "-");
+			}
+			else if (!txtCantidad.Text.Contains("."))
+			{
+				e.Handled = !char.IsDigit(e.KeyChar) & !char.IsControl(e.KeyChar) & !(Conversions.ToString(e.KeyChar) == ".");
+			}
+			else if (!txtCantidad.Text.Contains("-"))
+			{
+				e.Handled = !char.IsDigit(e.KeyChar) & !char.IsControl(e.KeyChar) & !(Conversions.ToString(e.KeyChar) == "-");
+			}
+			else if (txtCantidad.Text.Contains(".") & txtCantidad.Text.Contains("-"))
+			{
+				e.Handled = !char.IsDigit(e.KeyChar) & !char.IsControl(e.KeyChar);
+			}
+		}
+
+		private void btnOK_Click(object sender, EventArgs e)
+		{
+
+			if ((!string.IsNullOrEmpty(txtCantidad.Text)) && (!string.IsNullOrEmpty(txtCantidad.Text)))
+			{
+
+				if (LikeOperator.LikeString(txtCantidad.Text, "*.*-*", CompareMethod.Binary) | LikeOperator.LikeString(txtCantidad.Text, "0", CompareMethod.Binary) | LikeOperator.LikeString(txtCantidad.Text, "*-*", CompareMethod.Binary) | LikeOperator.LikeString(txtCantidad.Text, "*.*", CompareMethod.Binary) | LikeOperator.LikeString(txtCantidad.Text, "-", CompareMethod.Binary) | LikeOperator.LikeString(txtCantidad.Text, "-.", CompareMethod.Binary))
+				{
+					MessageBox.Show("Valor invalido");
+					return;
+				}
+
+
+				numInput = 0;
+				var notas = new string[(int)(Conversion.Val(txtCantidad.Text) - 1) + 1];
+				numCantidad = Convert.ToInt32(txtCantidad.Text);
+
+
+				foreach (var nota in notas)
+				{
+					numEntrada = Interaction.InputBox("Calificacion (base 10)" + ": ");
+					numInput = Conversion.Val(numEntrada);
+					if (Information.IsNumeric(numEntrada))
+					{
+						if (Conversions.ToDouble(numEntrada) > 10)
+						{
+							MessageBox.Show("Valores invalidos");
+							return;
+						}
+
+						notas[Convert.ToInt32(nota)] = Convert.ToString(numInput);
+					}
+					else
+					{
+						MessageBox.Show("Valores invalidos");
+						return;
+					}
+
+					numProm += Conversions.ToDouble(notas[Convert.ToInt32(nota)]);
+
+
+
+					
+				}
+
+				numProm = numProm / numCantidad;
+				MessageBox.Show(Convert.ToString(numProm));
+				var switchExpr = numProm;
+				switch (switchExpr)
+				{
+					case 10:
+						{
+							MessageBox.Show("AU");
+							break;
+						}
+
+					case object _ when 9 <= switchExpr && switchExpr <= 10:
+						{
+							MessageBox.Show("DE");
+							break;
+						}
+
+					case object _ when 8 <= switchExpr && switchExpr <= 9:
+						{
+							MessageBox.Show("SA");
+							break;
+						}
+
+					default:
+						{
+							MessageBox.Show("NA");
+							break;
+						}
+				}
+
+				
+			}
+			numProm = 0;
+		}
+	}
+}
