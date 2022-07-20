9c53f1cd30a09e3348801c84c8a9b1bec9a77ab9
data006 <109679683+data006@users.noreply.github.com>
2022-07-20 12:01:50 -0600

    Add files via upload

---
+++ Calificaciones con FOR VB/Calificaciones con FOR VB/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalificacionesFORVB
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(233, 67)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantidad.TabIndex = 0
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(243, 93)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 1
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(95, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(429, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Cantidad de calificaciones a promediar:"
+        '
+        'frmCalificacionesFORVB
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Yellow
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtCantidad)
+        Me.Name = "frmCalificacionesFORVB"
+        Me.Text = "Calificaciones"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents btnOK As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Calificaciones con FOR VB/Calificaciones con FOR VB/Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class frmCalificacionesFORVB
+
+    Dim numCantidad As Integer
+    Dim numInput As Double
+    Dim numProm As Double
+    Dim numEntrada As String
+
+
+    Private Sub txtCantidad_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtCantidad.KeyPress
+
+        If txtCantidad.Text = String.Empty Then
+
+            e.Handled = Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) And Not e.KeyChar = "." And Not e.KeyChar = "-"
+
+        ElseIf Not txtCantidad.Text.Contains(".") Then
+
+            e.Handled = Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) And Not e.KeyChar = "."
+
+        ElseIf Not txtCantidad.Text.Contains("-") Then
+
+            e.Handled = Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) And Not e.KeyChar = "-"
+
+        ElseIf txtCantidad.Text.Contains(".") And txtCantidad.Text.Contains("-") Then
+
+            e.Handled = Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar)
+
+        End If
+
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        If ((txtCantidad.Text IsNot String.Empty) And (txtCantidad.Text IsNot String.Empty)) Then
+
+            If txtCantidad.Text Like "*.*-*" Or txtCantidad.Text Like "0" Or txtCantidad.Text Like "*-*" Or txtCantidad.Text Like "*.*" Or txtCantidad.Text Like "-" Or txtCantidad.Text Like "-." Then
+
+                MsgBox("Valor invalido")
+                Exit Sub
+
+            End If
+
+            numInput = 0
+            Dim notas(txtCantidad.Text - 1) As String
+            numCantidad = Convert.ToInt32(txtCantidad.Text)
+
+
+
+
+
+            For Each nota As Integer In notas
+
+
+
+                numEntrada = InputBox("Calificacion (base 10)" & ": ")
+                numInput = Val(numEntrada)
+
+
+
+                If IsNumeric(numEntrada) Then
+
+                    If numEntrada > 10 Then
+
+                        MsgBox("Valores invalidos")
+                        Exit Sub
+                    End If
+
+                    notas(nota) = numInput
+
+
+
+                Else
+
+                    MsgBox("Valores invalidos")
+                    Exit Sub
+
+                End If
+
+                numProm += notas(nota)
+
+            Next
+
+            numProm = (numProm / numCantidad)
+            MsgBox(numProm)
+
+            Select Case numProm
+                Case 10
+                    MsgBox("AU")
+                Case 9 To 10
+                    MsgBox("DE")
+                Case 8 To 9
+                    MsgBox("SA")
+                Case Else
+                    MsgBox("NA")
+            End Select
+
+        End If
+
+        numProm = 0
+
+    End Sub
+End Class
